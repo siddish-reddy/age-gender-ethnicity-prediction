5c839ebe0283eff900fa355ef95077ef03d8906a
siddish-reddy <Siddish.ibv@gmail.com>
2019-09-22 09:53:10 +0530

    removed route and made dumy one

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from io import BytesIO
 
 from flask import Flask, make_response, request, render_template
 from werkzeug.exceptions import BadRequest
-from serving import (
-    load_model,
-    evaluate_input,
-)
+# from serving import (
+#     load_model,
+#     evaluate_input,
+# )
 
 app = Flask(__name__)
-app.config['DEBUG'] = False
-load_model()
+app.config['DEBUG'] = True
+# load_model()
 
 @app.route('/', methods=['GET'])
 def index():
@@ -20,7 +20,7 @@ def index():
 
 @app.route('/image', methods=["POST"])
 def eval_image():
-    """"Preprocessing the data and evaluate the model
+    # Preprocessing the data and evaluate the model
     # check if the post request has the file part
     input_file = request.files.get('file')
     if not input_file:
@@ -32,19 +32,16 @@ def eval_image():
 
     # # Save Image to process
     input_buffer = BytesIO()
-    output_buffer = BytesIO()
+    #output_buffer = BytesIO()
     input_file.save(input_buffer)
 
-    img = evaluate_input(input_buffer)
-    img.save(output_buffer, format="JPEG")
-    img_str = base64.b64encode(output_buffer.getvalue())
-
+    #img = evaluate_input(input_buffer)
+    #img.save(output_buffer, format="JPEG")
+    #img_str = base64.b64encode(output_buffer.getvalue())
+    img_str = base64.b64encode(input_buffer.getvalue())
     response = make_response(img_str)
     response.headers.set('Content-Type', 'image/jpeg')
     return response
-    """
-    return render_template('serving_template.html')
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=80, threaded=False)
